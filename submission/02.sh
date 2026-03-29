c2b1341b4677f689a82c0a75b69d0f2b7fa73ef6
ella <randomgive98@gmail.com>
2026-03-29 16:40:05 +0100

    02 fix removed named flag

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-segwitadd=$(bitcoin-cli -regtest -rpcwallet="builderswallet" getnewaddress "segwit" "bech32" )\
-bitcoin-cli -regtest -named getaddressinfo address="$segwitadd" | jq -r '.pubkey'
+segwitadd=$(bitcoin-cli -regtest -rpcwallet="builderswallet" getnewaddress "segwit" "bech32" )
+bitcoin-cli -regtest -rpcwallet="builderswallet" getaddressinfo "$segwitadd"  | jq -r '.pubkey'
